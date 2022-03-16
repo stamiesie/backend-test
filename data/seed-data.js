a2defe691bd11a5b1c367a21e951edbec07ad912
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2022-03-16 14:22:37 -0700

    create table, character model, GET controller

---
+++ data/seed-data.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name: 'Totoro',
+    film: 'My Friend Totoro',
+    age: '23',
+  },
+  {
+    name: 'Howl',
+    film: 'Howl\'s',
+    age: '20',
+  },
+  {
+    name: 'Nausicaa',
+    film: 'Nausicaa of the Valley of The Wind',
+    age: '19',
+  },
+];
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS characters CASCADE;
+
+CREATE TABLE characters (
+    character_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+    character_name TEXT NOT NULL,
+    character_film TEXT NOT NULL,
+    character_age VARCHAR (512) NOT NULL
+);
